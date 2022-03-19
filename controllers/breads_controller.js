5629faf7283b481afef139ae8984cb0044d81125
Mae Pena <maepena@Maes-MacBook-Pro.local>
2022-03-19 14:15:07 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,14 +3,15 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 // INDEX
-breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: Bread,
-        title: 'Index Page'
-      }
-    )
-})
+breads.get('/:arrayIndex', (req, res) => {
+    if (Bread[req.params.arrayIndex]) {
+      res.render('Show', {
+        bread:Bread[req.params.arrayIndex]
+      })
+    } else {
+      res.render('404')
+    }
+  })
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
