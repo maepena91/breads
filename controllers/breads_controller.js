48d71799490251fcd55a04ee6908a98f48f9b111
Mae Pena <maepena@Maes-MacBook-Pro.local>
2022-03-19 14:47:43 -0700

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -17,14 +17,15 @@ breads.get('/:arrayIndex', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
       res.render('Show', {
-        bread:Bread[req.params.arrayIndex]
+        bread:Bread[req.params.arrayIndex],
+        index: req.params.arrayIndex,
       })
     } else {
-      res.send('404')
+      res.render('404')
     }
   })
 
-  // CREATE
+
 // CREATE
 breads.post('/', (req, res) => {
     if (!req.body.image) {
@@ -43,5 +44,11 @@ breads.post('/', (req, res) => {
 breads.get('/new', (req, res) => {
     res.render('new')
 })
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+    Bread.splice(req.params.indexArray, 1)
+    res.status(303).redirect('/breads')
+  })
 
 module.exports = breads
